210d053f98e356695c8d469527556c2c0cc3e41d
Hypenexy <hypenexy@gmail.com>
2026-02-23 17:39:17 +0200

    Added more stuff to the razpis

--- razpisaniq/2kurs4potok2sem.js
+++ razpisaniq/2kurs4potok2sem.js
@@ -1,4 +1,5 @@
-// Vijte purviq semestur za detaili na kak se opisva programata
+// Ne gledaite purviq semestur za kak se opisva programata
+// poneje sintaksisa e strogo promenen
 
 var programa = {
     meta:{
@@ -25,6 +26,14 @@ var programa = {
         "лу": "laboratory",
         "с": "sport"
     },
+    groups:[
+        "41а",
+        "41б",
+        "42а",
+        "42б",
+        "43а",
+        "43б"
+    ],
     classes:{
         // lekcii
         "БД_л":{
@@ -84,17 +93,27 @@ var programa = {
             name: "Систематични системни",
             type: "су",
             room: 2102
+        },
+        //sport
+        "Спорт":{
+            name: "Спорт",
+            type: "с",
+            room: "АУ"
         }
     },
     1:{
         "БД_лу": [
             { hours: "3-4", groups: 3 },
-            { hours: "7-8", groups: 4 },
             { hours: "5-6", groups: 5 },
+            { hours: "7-8", groups: 4 },
+            { hours: "9-10", groups: 6 }
         ],
         "СС_су": [
             { hours: "5-6", groups: [3, 4] },
             { hours: "7-8", groups: [5, 6] }
+        ],
+        "Спорт": [
+            { hours: "3-4", groups: [5, 6] }
         ]
     },
     2:{
@@ -109,12 +128,59 @@ var programa = {
         "БД_л": [
             { hours: "11-12", weeks: "11-13" }
         ],
+        "БД_лу": [
+            { hours: "3-4", groups: 1 },
+            { hours: "5-6", groups: 2 }
+        ],
+        "ИИКТ_лу": [
+            { hours: "3-4", groups: 2, weeks: "3-10" },
+            { hours: "5-6", groups: 1, weeks: "3-10" }
+        ],
+        "Спорт": [
+            { hours: "3-4", groups: [3, 4], weeks: "2-13" },
+            { hours: "5-6", groups: [5, 6], weeks: "2-13" }
+        ]
     },
     3:{
-        
+        "СС_су": [
+            { hours: "2-3", groups: [1, 2], weeks: "3-10" }
+        ],
+        "ОМТ_л":[
+            { hours: "4-6", weeks: "1-10" }
+        ],
+        "КС_л":[
+            { hours: "4-6", weeks: "11-12" },
+            { hours: "9-10" }
+        ],
+        "БД_л":[
+            { hours: "7-8"}
+        ],
+        "БД_лу":[
+            { hours: "11-12", groups: 1, weeks: [4, 8, 12] },
+            { hours: "11-12", groups: 2, weeks: [5, 9, 13] }
+        ]
     },
     4:{
-
+        "ОМТ_лу":[
+            { hours: "3-4", groups: 1},
+            { hours: "5-6", groups: 2},
+            { hours: "7-8", groups: 5},
+            { hours: "9-10", groups: 6}
+        ],
+        "БД_лу":[
+            { hours: "11-12", groups: 3, weeks: [4, 6, 10] },
+            { hours: "11-12", groups: 4, weeks: [3, 7, 11] },
+            { hours: "11-12", groups: 5, weeks: [2, 8, 12] },
+            { hours: "11-12", groups: 6, weeks: [5, 9, 13] }
+        ],
+        "СС_лу":[
+            { hours: "7-8", groups: 3, weeks: "3-10" },
+            { hours: "9-10", groups: 4, weeks: "3-10" }
+        ],
+        "КС_лу":[
+            { hours: "3-4", groups: 2},
+            { hours: "5-6", groups: 1},
+        ]
     },
     5:{
 
